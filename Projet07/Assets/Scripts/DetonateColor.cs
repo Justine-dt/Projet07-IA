bbad6ead18ba914f02385536cf8da411e97bea40
Emilie Gonin <emilie.yuu@gmail.com>
2023-12-15 12:06:54 +0100

    add detonate color

---
+++ Projet07/Assets/Scripts/DetonateColor.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+
+public class DetonateColor : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer _sprite;
+
+    [SerializeField] private Color _hitColor = Color.black;
+    [SerializeField] private float _hitColorDuration = 0.2f;
+
+    private Color _normalColor;
+    private Coroutine _detonate;
+
+    private void Awake()
+    {
+        DetonateState.OnDetonate += ChangeColor;
+        DetonateState.OnStopDetonate += StopColor;
+        _normalColor = _sprite.color;
+    }
+
+    private void OnDestroy()
+    {
+        DetonateState.OnDetonate -= ChangeColor;
+        DetonateState.OnStopDetonate -= StopColor;
+    }
+
+    private void ChangeColor(Transform source)
+    {
+        if (source != transform) return;
+        _detonate = StartCoroutine(ColorChange());
+    }
+
+    private void StopColor(Transform source)
+    {
+        if (source != transform) return;
+        StopCoroutine(_detonate);
+    }
+
+    IEnumerator ColorChange()
+    {
+        while (true)
+        {
+            _sprite.color = _hitColor;
+            yield return new WaitForSeconds(_hitColorDuration);
+            _sprite.color = _normalColor;
+            yield return new WaitForSeconds(_hitColorDuration);
+            _hitColorDuration -= 0.01f;
+
+            if (_hitColorDuration <= 0) break;
+        }
+
+        yield break;
+    }
+}
