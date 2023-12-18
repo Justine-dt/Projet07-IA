56aaa933ee298b417094a30a739a4651d98bc657
Crimson41 <111068020+Crimson41@users.noreply.github.com>
2023-12-18 14:36:01 +0100

    stats speed

--- Projet07/Assets/Scripts/Brains/BatBrain.cs
+++ Projet07/Assets/Scripts/Brains/BatBrain.cs
@@ -1,7 +1,24 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BatBrain : Brain
 {
+    [SerializeField] float idleBatSpeed = 1.0f;
+    [SerializeField] float chaseBatSpeed = 5.0f;
 
+    protected override void Update()
+    {
+          base.Update();
+    }
+
+    protected override void ChangeState(State newState, GameObject target)
+    {
+        base.ChangeState(newState, target);
+
+        if (newState is ChaseState)
+        {
+            //
+        }
+    }
 }
