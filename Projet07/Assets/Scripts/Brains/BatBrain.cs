6db0e9684140fd5fbb853731ef9daa76890d779d
Crimson41 <111068020+Crimson41@users.noreply.github.com>
2023-12-18 15:03:36 +0100

    bat swapsprite

--- Projet07/Assets/Scripts/Brains/BatBrain.cs
+++ Projet07/Assets/Scripts/Brains/BatBrain.cs
@@ -1,24 +1,50 @@
-using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class BatBrain : Brain
 {
+    private static bool isAnyBatInChaseState = false;
+    private Animator batAnimator;
 
-    protected override void Update()
+    private void Start()
     {
-          base.Update();
+        batAnimator = GetComponent<Animator>();
     }
 
     protected override void ChangeState(State newState, GameObject target)
     {
         base.ChangeState(newState, target);
 
-        //if (newState is ChaseState)
-        //{
-        //    Bat[] bats = FindObjectOfType<Bat>
-        //}
+        if (newState is ChaseState chaseState)
+        {
+            if (!isAnyBatInChaseState)
+            {
+                isAnyBatInChaseState = true;
+                ChangeSpriteAnimation("Bat_Angry_Idle_Move");
+                ChangeAllBatBrainsState(new ChaseState(), target);
+            }
+        }
+        else
+        {
+            isAnyBatInChaseState = false;
+            ChangeSpriteAnimation("Bat_Idle_Move");
+        }
+    }
 
-        //foreach (Bat)
+    private void ChangeAllBatBrainsState(State state, GameObject target)
+    {
+        BatBrain[] allBatBrains = FindObjectsOfType<BatBrain>();
+
+        foreach (BatBrain batBrain in allBatBrains)
+        {
+            batBrain.ChangeState(state, target);
+        }
+    }
+
+    private void ChangeSpriteAnimation(string animationName)
+    {
+        if (batAnimator != null)
+        {
+            batAnimator.Play(animationName);
+        }
     }
 }
