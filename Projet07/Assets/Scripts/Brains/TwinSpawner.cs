e25ef6d57ef085c742c4793ded26972dff3db974
Emilie Gonin <emilie.yuu@gmail.com>
2023-12-19 17:33:48 +0100

    add blob and twins

---
+++ Projet07/Assets/Scripts/Brains/TwinSpawner.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class TwinSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _twinAPrefab;
+    [SerializeField] private GameObject _twinBPrefab;
+
+    private GameObject _twinA;
+    private GameObject _twinB;
+    private TwinBrain _twinABrain;
+    private TwinBrain _twinBBrain;
+
+    private void Awake()
+    {
+        _twinA = Instantiate(_twinAPrefab);
+        _twinB = Instantiate(_twinBPrefab);
+
+        _twinABrain = _twinA.GetComponent<TwinBrain>();
+        _twinBBrain = _twinB.GetComponent<TwinBrain>();
+
+        _twinABrain.SetTwin(_twinBBrain);
+        _twinBBrain.SetTwin(_twinABrain);
+    }
+}
