b8f894aa3e874777631dcb93baf9dfe00410856b
TheoBrs <tboros@gaming.tech>
2023-12-20 15:23:59 +0100

    Work

--- Projet07/Assets/Scripts/BossAI/BossBT.cs
+++ Projet07/Assets/Scripts/BossAI/BossBT.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class GuardBT : BehaviorTree.Tree
 {
-    public Transform _bossTransform;
-    public SpriteRenderer _bossRenderer;
-    public float speed = 2f;
+    [SerializeField] Transform _bossTransform;
+    [SerializeField] SpriteRenderer _bossRenderer;
+    [SerializeField] float speed = 2f;
+
+    [SerializeField] List<GameObject> _enemyprefabs;
 
     public static bool hasPlayerEnteredRoom = false;
 
     
     protected override Node SetupTree()
     {
-        /*
-        Node root = new Sequence(new List<Node>
+        
+        Node root = new Selector(new List<Node>
         {
             new BossMovement(_bossTransform, _bossRenderer),
-            new BossSpawnAllies(_bossTransform)
+            new BossSpawnAllies(_bossTransform, _enemyprefabs)
         });
-        */
+        
 
-        Node root = new BossMovement(_bossTransform, _bossRenderer);
+        //Node root = new BossMovement(_bossTransform, _bossRenderer);
 
         return root;
     }
--- Projet07/Assets/Scripts/BossAI/BossSpawnAllies.cs
+++ Projet07/Assets/Scripts/BossAI/BossSpawnAllies.cs
@@ -7,15 +7,27 @@ using BehaviorTree;
 public class BossSpawnAllies : Node
 {
     Transform _bossTransform;
+    List<GameObject> _enemyPrefabs;
 
-    public BossSpawnAllies(Transform transform)
+    
+
+    public BossSpawnAllies(Transform transform, List<GameObject> prefabs)
     {
         _bossTransform = transform;
+        _enemyPrefabs = prefabs;
     }
 
     public override NodeState Evaluate()
     {
-        //Instantiate 4 prefabs aléatoires dans une liste prédéfinies de chaque côté du boss
+        //Instantiate 4 prefabs aléatoires dans une liste prédéfinies de chaque côté du boss s'il y a strictement moins de 8 ennemis dans la salle
+        if(GameObject.FindGameObjectsWithTag("Enemy").Length < 8)
+        {
+            int randomNumber = Random.Range(0, _enemyPrefabs.Count);
+            //Instantiate(_enemyPrefabs[randomNumber], (_bossTransform.position + new Vector3(3, 0, 0)), Quaternion.identity);
+            Debug.Log("Spawn enemies");
+        }
+
+
 
         Debug.Log("yiipeeeee");
 
