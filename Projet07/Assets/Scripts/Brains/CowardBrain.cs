6463f34f80618dc5c20146b7fdb2e11855ead1d5
Emilie Gonin <emilie.yuu@gmail.com>
2023-12-22 01:33:00 +0100

    fix coward ia

--- Projet07/Assets/Scripts/Brains/CowardBrain.cs
+++ Projet07/Assets/Scripts/Brains/CowardBrain.cs
@@ -1,12 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class CowardBrain : Brain
 {
-    // 3 Differents States 
-    // Idle State
-    // Chase State
-    // RunAway State
-
+    //
 }
